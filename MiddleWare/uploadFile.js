b8625641721d59b974758e226111f9ea1bbd34c2
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-12 13:55:30 +0545

    done with uploadfile middleware

---
+++ MiddleWare/uploadFile.js
@@ -0,0 +1,25 @@
+const multer = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/files");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + 'NewWriters' + file.originalname);
+    },
+  });
+const maxSize = 20 * 1024 * 1024; // for 1MB
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    console.log(file )
+    if ( file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||file.mimetype == "application/vnd.openxmlformats-officedocument.presentationml.presentation" || file.mimetype == "application/zip" ||file.mimetype=="file/pptx" || file.mimetype == "application/pdf" || file.mimetype == "file/pdf") {
+      cb(null, true);
+    } else {
+      cb({success: false,
+        msg: 'Invalid file type. Only jpg, png image files are allowed.'},false);
+    }
+  },
+  limits: { fileSize: maxSize },
+}).single('file');
+module.exports = upload
